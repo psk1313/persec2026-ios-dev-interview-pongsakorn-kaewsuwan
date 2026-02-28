ee21d84893a56aafb65d4ba9d7f708b73d7d6e59
Pongsakorn Kaewsuwan <pongsakornk@phillip.co.th>
2026-02-28 14:28:05 +0700

    setup project

---
+++ PersecPreInterview/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PersecPreInterview
+//
+//  Created by Pongsakorn Kaewsuwan on 28/2/2569 BE.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ PersecPreInterviewTests/PersecPreInterviewTests.swift
@@ -0,0 +1,16 @@
+//
+//  PersecPreInterviewTests.swift
+//  PersecPreInterviewTests
+//
+//  Created by Pongsakorn Kaewsuwan on 28/2/2569 BE.
+//
+
+import Testing
+
+struct PersecPreInterviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
