33bc170b4d4920f9564b18fc65d22c0e1c6bf248
Pongsakorn Kaewsuwan <pongsakornk@phillip.co.th>
2026-02-28 16:05:06 +0700

    create BracketValidator

---
+++ PersecPreInterview/Algorithms/BracketValidator.swift
@@ -0,0 +1,32 @@
+//
+//  BracketValidator.swift
+//  PersecPreInterview
+//
+//  Created by Pongsakorn Kaewsuwan on 28/2/2569 BE.
+//
+
+import Foundation
+
+struct BracketValidator {
+    
+    static func isValid(_ input: String) -> Bool {
+        guard !input.isEmpty,
+              (input.count % 2) == .zero else {
+            return false
+        }
+        
+        var stack: [Character] = []
+        let pairs: [Character: Character] = [")" : "(",
+                                             "]" : "[",
+                                             "}" : "{"]
+        for char in input {
+            if let expectedOpen = pairs[char] {
+                guard stack.last == expectedOpen else { return false }
+                stack.removeLast()
+            } else {
+                stack.append(char)
+            }
+        }
+        return stack.isEmpty
+    }
+}
