2559298e6f460d3166d2cefda199a68b5dda2a43
Pongsakorn Kaewsuwan <pongsakornk@phillip.co.th>
2026-02-28 18:08:52 +0700

    create Autocomplete

---
+++ PersecPreInterview/Algorithms/Autocomplete.swift
@@ -0,0 +1,37 @@
+//
+//  Autocomplete.swift
+//  PersecPreInterview
+//
+//  Created by Pongsakorn Kaewsuwan on 28/2/2569 BE.
+//
+
+import Foundation
+
+struct Autocomplete {
+
+    static func autocomplete(_ keywords: String,
+                             items: [String],
+                             maxResult: Int) -> [String] {
+
+        guard !keywords.isEmpty,
+              maxResult > 0 else {
+            return []
+        }
+
+        var filtered: [(String, Int)] = []
+        let keywordsLower = keywords.lowercased()
+
+        for item in items {
+            guard filtered.count < maxResult else { break }
+            
+            let lower = item.lowercased()
+            guard let range = lower.range(of: keywordsLower) else { continue }
+
+            let position = lower.distance(from: lower.startIndex, to: range.lowerBound)
+            filtered.append((item, position))
+        }
+        
+        let sorted = filtered.sorted { $0.1 < $1.1 }.map { $0.0 }
+        return sorted
+    }
+}
