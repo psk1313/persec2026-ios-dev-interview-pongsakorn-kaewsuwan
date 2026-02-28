30cb672d9131baba6b2c19f6518d635a0a32e9a3
Pongsakorn Kaewsuwan <pongsakornk@phillip.co.th>
2026-02-28 19:19:30 +0700

    create PositiveInt

---
+++ PersecPreInterview/Algorithms/PositiveInt.swift
@@ -0,0 +1,14 @@
+//
+//  PositiveInt.swift
+//  PersecPreInterview
+//
+//  Created by Pongsakorn Kaewsuwan on 28/2/2569 BE.
+//
+
+struct PositiveInt {
+    
+    static func sortDescending(_ number: Int) -> Int {
+        let sortedString = String(number).sorted(by: >)
+        return Int(String(sortedString)) ?? 0
+    }
+}
