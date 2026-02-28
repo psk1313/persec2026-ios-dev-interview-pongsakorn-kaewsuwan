4cc533133f16f159d91e95313b6c1fa1661101f8
Pongsakorn Kaewsuwan <pongsakornk@phillip.co.th>
2026-02-28 16:46:22 +0700

    update ArraySorterTestCase

--- PersecPreInterview/main.swift
+++ PersecPreInterview/main.swift
@@ -5,8 +5,7 @@
 //  Created by Pongsakorn Kaewsuwan on 28/2/2569 BE.
 //
 
-import Foundation
-
+/// ข้อที่ 1
 let bracketValidatorTestCase: [String] = ["()",
                           "([]]",
                           "([{}])",
@@ -17,5 +16,14 @@ let bracketValidatorTestCase: [String] = ["()",
                           "{"]
 
 for testCase in bracketValidatorTestCase {
-    print("\(testCase) => \(BracketValidator.isValid(testCase))")
+    print("\(testCase) ==> \(BracketValidator.isValid(testCase))")
+}
+
+
+/// ข้อที่ 2
+let arraySorterTestCase: [[String]] = [["TH19", "SG20" , "TH2"],
+                                       ["TH10", "TH3Netflix" , "TH1", "TH7"]]
+
+for testCase in arraySorterTestCase {
+    print("\(testCase) ==> \(ArraySorter.sortCodes(testCase))")
 }
